ce46e62854ca9c3ca1507ccbcd7ba9b1397f0fa4
Zach <zachgurwitz@gmail.com>
2024-11-06 18:21:51 -0500

    FIrst commit

---
+++ top.sv
@@ -0,0 +1,75 @@
+`include "Sysbus.defs"
+
+module top
+#(
+  ID_WIDTH = 13,
+  ADDR_WIDTH = 64,
+  DATA_WIDTH = 64,
+  STRB_WIDTH = DATA_WIDTH/8
+)
+(
+  input  clk,
+         reset,
+         hz32768timer,
+
+  // 64-bit addresses of the program entry point and initial stack pointer
+  input  [63:0] entry,
+  input  [63:0] stackptr,
+  input  [63:0] satp,
+
+  // interface to connect to the bus
+  output  wire [ID_WIDTH-1:0]    m_axi_awid,
+  output  wire [ADDR_WIDTH-1:0]  m_axi_awaddr,
+  output  wire [7:0]             m_axi_awlen,
+  output  wire [2:0]             m_axi_awsize,
+  output  wire [1:0]             m_axi_awburst,
+  output  wire                   m_axi_awlock,
+  output  wire [3:0]             m_axi_awcache,
+  output  wire [2:0]             m_axi_awprot,
+  output  wire                   m_axi_awvalid,
+  input   wire                   m_axi_awready,
+  output  wire [DATA_WIDTH-1:0]  m_axi_wdata,
+  output  wire [STRB_WIDTH-1:0]  m_axi_wstrb,
+  output  wire                   m_axi_wlast,
+  output  wire                   m_axi_wvalid,
+  input   wire                   m_axi_wready,
+  input   wire [ID_WIDTH-1:0]    m_axi_bid,
+  input   wire [1:0]             m_axi_bresp,
+  input   wire                   m_axi_bvalid,
+  output  wire                   m_axi_bready,
+  output  wire [ID_WIDTH-1:0]    m_axi_arid,
+  output  wire [ADDR_WIDTH-1:0]  m_axi_araddr,
+  output  wire [7:0]             m_axi_arlen,
+  output  wire [2:0]             m_axi_arsize,
+  output  wire [1:0]             m_axi_arburst,
+  output  wire                   m_axi_arlock,
+  output  wire [3:0]             m_axi_arcache,
+  output  wire [2:0]             m_axi_arprot,
+  output  wire                   m_axi_arvalid,
+  input   wire                   m_axi_arready,
+  input   wire [ID_WIDTH-1:0]    m_axi_rid,
+  input   wire [DATA_WIDTH-1:0]  m_axi_rdata,
+  input   wire [1:0]             m_axi_rresp,
+  input   wire                   m_axi_rlast,
+  input   wire                   m_axi_rvalid,
+  output  wire                   m_axi_rready,
+  input   wire                   m_axi_acvalid,
+  output  wire                   m_axi_acready,
+  input   wire [ADDR_WIDTH-1:0]  m_axi_acaddr,
+  input   wire [3:0]             m_axi_acsnoop
+);
+
+  logic [63:0] pc;
+
+  always_ff @ (posedge clk)
+    if (reset) begin
+      pc <= entry;
+    end else begin
+      $display("Hello World!  @ %x", pc);
+      $finish;
+    end
+
+  initial begin
+    $display("Initializing top, entry point = 0x%x", entry);
+  end
+endmodule
