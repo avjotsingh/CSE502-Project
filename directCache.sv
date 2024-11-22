c91e8aec7e5eafe7da67bca0381e3797aacd40b5
Zach <zachgurwitz@gmail.com>
2024-11-21 23:24:17 -0500

    cache next_state logic

--- directCache.sv
+++ directCache.sv
@@ -10,10 +10,14 @@ module directCache
 (
     input wire clk,
     input wire reset,
-    input wire [ADDR_WIDTH-1:0] addr,
-    input wire arvalid,
+    
+    input wire avalid,
+    input wire [ADDR_WIDTH-1:0] aaddr,
+    //if not, then store
     input wire load,
-    output wire arready,
+
+    input wire dready,
+
     output wire dvalid,
     output wire [DATA_WIDTH-1:0] data_out
 );
@@ -26,49 +30,83 @@ module directCache
     assign hit = tag == cache[index][TAG_LENGTH-1:0];
     assign data_out = hit ? cache[index][(offset + 1) * DATA_WIDTH + STATE_BITS + TAG_LENGTH - 1: offset * DATA_WIDTH + STATE_BITS + TAG_LENGTH] : 1 /*this is a debug bit rn*/;
 
-    enum {RESET, STANDBY, LOAD, STORE, MISS} state, next_state;	
+    //did_we_just_finish_our_memory_operation? probably replace with a wire that communicates with the bus
+    wire mod;
+
+    enum {IDLE, OUTPUT, IDLE_BUSY, OUTPUT_BUSY, LOADING, STALL_LOAD, STALL_STORE} state, next_state;	
     logic [2**INDEX_LENGTH-1:0] [DATA_WIDTH * (2**OFFSET_LENGTH) + STATE_BITS + TAG_LENGTH - 1:0] cache;
     
     always_ff @ (posedge clk) begin
-        case (state)
-            RESET: internal_addr <= 0;
-            STANDBY: internal_addr <= addr;
-            LOAD: begin 
-                internal_addr <= internal_addr;
-            end
-            STORE: internal_addr <= internal_addr;
-            MISS: internal_addr <= internal_addr;
-        endcase
+        if (reset) begin
+        end else begin
+            case (state)
+                IDLE: begin
+                    
+                end
+                OUTPUT: begin
+                    
+                end
+                IDLE_BUSY: begin
+                    
+                end
+                OUTPUT_BUSY: begin
+                    
+                end
+                LOADING: begin
+                    
+                end
+                STALL_LOAD: begin
+                    
+                end
+                STALL_STORE: begin
+                    
+                end
+            endcase
+        end
     end
 
     //output logic
     always_comb begin
         case (state)
-        RESET: begin
-            arready = 0; 
-            dvalid = 0;
-            data_out = 0;
-        end
-        STANDBY : begin 
-            arready = 1;
-            dvalid = 0;
-            data_out = 0;
-        end
-        LOAD : begin
-            cache[]
-        end
+            IDLE: begin
+                
+            end
+            OUTPUT: begin
+                
+            end
+            IDLE_BUSY: begin
+                
+            end
+            OUTPUT_BUSY: begin
+                
+            end
+            LOADING: begin
+                
+            end
+            STALL_LOAD: begin
+                
+            end
+            STALL_STORE: begin
+                
+            end
         endcase
     end
 
     //next state logic
     always_comb begin
         case (state)
-        RESET: next_state = reset ? RESET : STANDBY;
-        STANDBY: next_state = arvalid ? (load ? LOAD : STORE) : STANDBY;
-        LOAD: next_state = miss ? MISS : STANDBY;
-        STORE: next_state = miss ? MISS : STANDBY;
-        MISS: next_state = found ? STANDBY : MISS;
-
+            IDLE: next_state = avalid ? (hit ? (load ? OUTPUT : IDLE) : (load ? OUTPUT_BUSY : IDLE_BUSY)) : IDLE; 
+            OUTPUT: next_state = dready ? IDLE : OUTPUT;
+            IDLE_BUSY: next_state = mod ? (avalid ? (hit ? (load ? OUTPUT : IDLE) : (load ? OUTPUT_BUSY : IDLE_BUSY)) : IDLE) : 
+                (avalid ? (hit ? (load ? OUTPUT_BUSY : IDLE_BUSY) : (load ? STALL_LOAD : STALL_STORE)) : IDLE_BUSY); 
+            OUTPUT_BUSY: next_state = mod ? (dready ? IDLE : OUTPUT) : (dready ? IDLE_BUSY : OUTPUT_BUSY);
+            //TODO: replace with a wire that (is connected to/communicates with) the memory bus ≡ rdone
+            // next_state = rdone ? OUTPUT_BUSY : LOADING;
+            LOADING: next_state = OUTPUT_BUSY;
+            //TODO: I think I'm overloading the load variable here, maybe need another register that remembers the previous
+            //FIX: Add a new state to differentiate the two
+            STALL_LOAD: next_state = mod ? LOADING : STALL_LOAD; 
+            STALL_STORE: next_state = mod ? IDLE_BUSY : STALL_STORE; 
         endcase
     end
 
